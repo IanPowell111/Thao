88790a580a155c5ca0f7d67690f41405e2ba2a2a
Vasylenko0908 <devstack777@gmail.com>
2023-04-07 03:27:34 +0200

    init

---
+++ portfolio.js
@@ -0,0 +1,356 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+	name: "Thao Tran",
+	title: "Electronic & Mobile App Developer",
+	description:[
+			"- Programming skills in VHDL , C and C++, Java, Objective-c, Swift, Kotlin, Flutter and React Native",
+		],
+	resumeLink:
+		"https://drive.google.com/file/d/1KbCV08ZdwJW7NnR09JWujBJcVdJirxzok/view?usp=share_link",
+	Email: "Email: tran.thaothao520@gmail.com",
+	Phone: "Phone number: +84 977628774",
+	Address: "Address: 112 Huynh Huu Thong Street, Tan An, Vietnam, 850000"
+};
+
+export const openSource = {
+	githubUserName: "Thao Tran",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+	// url: "https://app.joinrise.co/professional/shibayashihirobm7av",
+	// linkedin: "https://shibayashihiro.vercel.app",
+	github: "https://github.com/fullstackdev116",
+	// instagram: "https://shibayashihiro.vercel.app",
+	// facebook: "https://shibayashihiro.vercel.app",
+	// twitter: "https://shibayashihiro.vercel.app",
+};
+
+export const skillsSection = {
+	title: "What I do",
+	subTitle:
+		"Electronics & Mobile Developer",
+	data: [
+		{
+			title: "Electronic & Mobile Programming",
+			lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji(
+					"⚡ Building responsive Single-Page-Apps (SPA) & PWA in React.js"
+				),
+				emoji("⚡ Building responsive static websites using Next.js"),
+				emoji(
+					"⚡ Building RESTful APIs in Django & Django REST Framework"
+				),
+			],
+			softwareSkills: [
+				{
+					skillName: "HTML-5",
+					fontAwesomeClassname: "vscode-icons:file-type-html",
+				},
+				{
+					skillName: "CSS-3",
+					fontAwesomeClassname: "vscode-icons:file-type-css",
+				},
+				{
+					skillName: "JavaScript",
+					fontAwesomeClassname: "logos:javascript",
+				},
+				{
+					skillName: "Java",
+					fontAwesomeClassname: "logos:java",
+				},
+				{
+					skillName: "PHP",
+					fontAwesomeClassname: "vscode-icons:file-type-php",
+				},
+				{
+					skillName: "mysql",
+					fontAwesomeClassname: "vscode-icons:file-type-mysql",
+				},
+				{
+					skillName: "Laravel",
+					fontAwesomeClassname: "logos:laravel",
+				},
+				{
+					skillName: "Yii",
+					fontAwesomeClassname: "logos:yii",
+				},
+				{
+					skillName: "CodeIgniter",
+					fontAwesomeClassname: "logos:codeigniter",
+				},
+				{
+					skillName: "WordPress",
+					fontAwesomeClassname: "logos:wordpress",
+				},
+				{
+					skillName: "Magento",
+					fontAwesomeClassname: "logos:magento",
+				},
+				{
+					skillName: "Drupal",
+					fontAwesomeClassname: "logos:drupal",
+				},
+				{
+					skillName: "Joomla",
+					fontAwesomeClassname: "logos:joomla",
+				},
+				{
+					skillName: "Reactjs",
+					fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+				},
+				{
+					skillName: "Vuejs",
+					fontAwesomeClassname: "logos:vue",
+				},
+				{
+					skillName: "Python",
+					fontAwesomeClassname: "logos:python",
+				},
+				{
+					skillName: "Django",
+					fontAwesomeClassname: "vscode-icons:file-type-django",
+				},
+				{
+					skillName: "Redux",
+					fontAwesomeClassname: "logos:redux",
+				},
+				{
+					skillName: "NPM",
+					fontAwesomeClassname: "logos:npm-icon",
+				},
+				{
+					skillName: "Yarn",
+					fontAwesomeClassname: "logos:yarn",
+				},
+			],
+		},
+		{
+			title: "Cloud Infra-Architecture",
+			lottieAnimationFile: "/lottie/skills/cloudinfra.json", // Path of Lottie Animation JSON File
+			skills: [
+				emoji("⚡ Experience of working on multiple cloud platforms"),
+				emoji(
+					"⚡ Hosting and maintaining websites on virtual machine instances along with integration of databases"
+				),
+				emoji(
+					"⚡ Building CI/CD pipelines for automated testing & deployment using Github Actions"
+				),
+			],
+			softwareSkills: [
+				// ? Check README To get icon details
+				{
+					skillName: "AWS",
+					fontAwesomeClassname: "logos:aws",
+				},
+				{
+					skillName: "Azure",
+					fontAwesomeClassname: "logos:microsoft-azure",
+				},
+				{
+					skillName: "Heroku",
+					fontAwesomeClassname: "logos:heroku-icon",
+				},
+				{
+					skillName: "PostgreSQL",
+					fontAwesomeClassname: "logos:postgresql",
+				},
+				{
+					skillName: "Github",
+					fontAwesomeClassname: "akar-icons:github-fill",
+				},
+				{
+					skillName: "Docker",
+					fontAwesomeClassname: "logos:docker-icon",
+				},
+				{
+					skillName: "Github Actions",
+					fontAwesomeClassname: "logos:github-actions",
+				},
+				{
+					skillName: "Cloudinary",
+					fontAwesomeClassname: "logos:cloudinary",
+				},
+				{
+					skillName: "Nginx",
+					fontAwesomeClassname: "logos:nginx",
+				},
+				{
+					skillName: "Sentry",
+					fontAwesomeClassname: "logos:sentry-icon",
+				},
+			],
+		},
+		// {
+		// 	title: "Blockchain",
+		// 	lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+		// 	skills: [
+		// 		emoji(
+		// 			"⚡ Experience in developing Smart Contract using Solidity & Ethereum"
+		// 		),
+		// 		emoji(
+		// 			"⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura"
+		// 		),
+		// 		emoji(
+		// 			"⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles"
+		// 		),
+		// 		emoji(
+		// 			"⚡ Developing NFT Smart Contracts using ERC-721 Token Standard"
+		// 		),
+		// 		emoji(
+		// 			"⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS"
+		// 		),
+		// 	],
+		// 	softwareSkills: [
+		// 		{
+		// 			skillName: "Ethereum",
+		// 			fontAwesomeClassname: "logos:ethereum",
+		// 		},
+		// 		{
+		// 			skillName: "Solidity",
+		// 			fontAwesomeClassname: "logos:solidity",
+		// 		},
+		// 		{
+		// 			skillName: "Web3js",
+		// 			fontAwesomeClassname: "logos:web3js",
+		// 		},
+		// 		{
+		// 			skillName: "Metamask",
+		// 			fontAwesomeClassname: "logos:metamask-icon",
+		// 		},
+		// 		{
+		// 			skillName: "Ganache",
+		// 			fontAwesomeClassname: "logos:ganache-icon",
+		// 		},
+		// 	],
+		// },
+	],
+};
+
+export const SkillBars = [
+	{
+		Stack: "System Architecture", //Insert stack or technology you have experience in
+		progressPercentage: "90", //Insert relative proficiency in percentage
+	},
+	{
+		Stack: "Mobile Programming",
+		progressPercentage: "90",
+	},
+	{
+		Stack: "Electronic Design",
+		progressPercentage: "93",
+	},
+	{
+		Stack: "Programming",
+		progressPercentage: "89",
+	},
+];
+
+export const educationInfo = [
+	{
+		schoolName: "Tan An Science College",
+		subHeader: "Master of Science in Computer Science",
+		duration: "April 2012 - July 2015",
+		desc: "Relevant Coursework:",
+		grade: "Grade A",
+		descBullets: [
+			"System Architecture",
+			"Linux/Unix Programming",
+			"Usability in Website and Software Design",
+			"C++ Programming I & II",
+			"Mobile App Development",
+			"Relational Database Design & SQL",
+			"Data Engineering",
+			"Advanced Software Programming",
+
+		],
+	},
+];
+
+export const experience = [
+	{
+		role: "Mobile App Developer",
+		company: "Ediattah",
+		companylogo: "/img/icons/common/247_labs.png",
+		date: "Dec 2017 – Mar 2023",
+		desc: "Development of social & ecommerce mobile apps",
+		descBullets: [
+			"Developed full-stack web & mobile applications which processed, analyzed, and rendered data visually.",
+			"Liaised with back end developers, front end developers, quality assurance testers, and CTO as needed.",
+			"Managed time-sensitive updates, including content changes and database upgrades.",
+			"Planned, wrote, and debugged web applications and software with complete accuracy.",
+			"Implemented websites, mobile applications, and landing pages from concept through deployment.",
+			"Standardized all output with a new, responsive, mobile-first approach and strategy.",
+			"Assessed UX and UI designs for technical feasibility.",
+			"Developed standard and ad hoc reports in graph format as required.",
+			"Collaborated with product team members to implement new feature developments.",
+		],
+		
+	},
+	{
+		role: "Hardware Engineer",
+		company: "Saritasa",
+		companylogo: "/img/icons/common/saritasa.png",
+		date: "Sep 2015 – Jul 2017",
+		desc: "Electronic programming",
+		descBullets: [
+			" Created and used a prototype library for Microchip programming & electronic board.",
+			" Developed firmware program by using c language",
+		],
+	},
+	
+];
+
+export const projects = [
+	{
+		name: "Divina Transport",
+		desc: "Taxi-booking mobile app and web panel",
+		github: "https://github.com/fullstackdev116/DivinaTransport",
+		link: "https://divinatransport.ci",
+	},
+	{
+		name: "Bluetooth USB Dongle controller",
+		desc: "Bluetooth usb dongle mobile app and firmware program",
+		github: "https://github.com/fullstackdev116/bledongle_android",
+		link: "",
+	},
+];
+
+export const feedbacks = [
+	{
+		name: "Ediattah Salomon",
+		feedback:
+			"Excellent and timely communication. Work was done very well, high technical knowledge. ",
+	},
+	{
+		name:"Jorge D.",
+		feedback:
+			"Working with Shiba was great. He was accessible and extremely collaborative on this project. He completed all milestones quickly and ahead of schedule. I will definitely go back to him for my next project.",
+	},
+	{
+		name:"Cristi B.",
+		feedback:
+			"Shiba was punctual, creative and hard-working. He's a great lovely peep to work with!"
+	}
+];
+
+// option to hide or show the ContactUs component
+export const showContactUs = true;
+
+// See object prototype on SEO.jsx page
+export const seoData = {
+	title: "Thao Tran",
+	description:
+		"Electronic and Mobile Engineer",
+	author: "Thao Tran",
+	image: "https://avatars3.githubusercontent.com/u/59178380?v=4",
+	url: "https://thaotran.vercel.app",
+	keywords: [
+		"Thao Tran",
+		"electronics",
+		"mobile app development",
+	],
+}
